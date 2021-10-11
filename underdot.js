f8d1f004d285c6ca53ade4eaa650f1d478e46971
AR <74219764+AR-Student824@users.noreply.github.com>
2021-10-11 03:42:24 +0000

    '

--- underdot.js
+++ underdot.js
@@ -1,14 +1,21 @@
 console.log('ehhlo')
 function _copy(text) {
     if (!typeof data == 'string') {
-        console.error('underdot >> error parsing data in _.copy()')
+        console.error('underdot >> error parsing data in _copy()')
     } else {
             var element = document.createElement("INPUT");
             element.setAttribute("value", text)
             document.body.appendChild(element);
             element.select();
-            //element.setSelectionRange(0, 99999);
             document.execCommand("copy");
             element.remove();
+    }
+}
+
+function _removeFromArray(array, data) {
+    if (!typeof array == 'array') {
+        console.error('underdot >> error parsing array')
+    } else {
+        return array.filter(i => i == data)
     }
 }
