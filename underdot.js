8ae08db94839da4bd1a935d90e3c75f98222721b
AR <74219764+AR-Student824@users.noreply.github.com>
2021-11-19 19:05:23 -0800

    ;

--- underdot.js
+++ underdot.js
@@ -1,10 +1,10 @@
 function _() {
-    return Object.keys(this).forEach(e => {console.log(e)})
+    return Object.keys(this).forEach(e => { console.log(e) })
 }
 
-var _c = function(text) {
+var _c = function (text) {
     if (window && document) {
-    try {
+        try {
             var element = document.createElement("INPUT");
             element.setAttribute("value", text)
             document.body.appendChild(element);
@@ -12,79 +12,91 @@ var _c = function(text) {
             document.execCommand("copy");
             element.remove();
             return text;
-    } catch {
-        console.error('underdot >> failed to copy text')
-    }
-} else {
-    console.error('underdot >> must be run in a browser')
-}
-}
-
-var _r = function(array, data) {
-    try {
-        return array.filter(i => i != data)
-    } catch {
-        console.error('underdot >> failed to remove from array')
-    }
-}
-
-var _ehtml = function(data) {
-try {
-  return data.replace(
-    /[&<>'"]/g,
-    tag =>
-      ({
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        "'": '&#39;',
-        '"': '&quot;',
-        "'": '&apos;'
-      }[tag] || tag)
-  );
-    } catch {
-        console.error('underdot >> failed to escape html')
-    }
-}
-
-var _fjson = function(data) {
-    try {
-        return JSON.stringify(data, null, "\t")
-    } catch {
-        console.error('underdot >> failed to format json')
-    }
-}
-
-var _l = function(url, code) {
-    if (typeof url == 'string' && (url.startsWith('https://') || url.startsWith('http://')) && code && typeof code == 'string') {
-        try {
-            fetch(url).then(response => response.text()).then(response => {
-                return eval(`${response}\n\n${code}`)
-            })
         } catch {
-            console.error('underdot >> failed to load script')
+            console.error('underdot >> failed to copy text')
         }
     } else {
-        console.error('underdot >> not a valid script url or code')
-    }   
+        console.error('underdot >> must be run in a browser')
+    }
 }
 
-var _prxy = function(url) {
-    if (typeof url == 'string' && (url.startsWith('https://') || url.startsWith('http://'))) {
-        return 'https://proxy.blobby.me/?reqUrl=' + encodeURIComponent(url)
-} else {
-    console.error('underdot >> not a valid url')
-}   
+var _rmv = function (array, data) {
+    return new Promise((resolve, reject) => {
+        try {
+            resolve(array.filter(i => i != data))
+        } catch {
+            reject('underdot >> failed to remove from array')
+        }
+    })
 }
 
-var _r = function(url) {
-    if (typeof url == 'string' && (url.startsWith('https://') || url.startsWith('http://'))) {
-        return window.location.href=url
-} else {
-    console.error('underdot >> not a valid url')
-}   
+var _ehtml = function (data) {
+    return new Promise((resolve, reject) => {
+        try {
+            resolve(data.replace(
+                /[&<>'"]/g,
+                tag =>
+                ({
+                    '&': '&amp;',
+                    '<': '&lt;',
+                    '>': '&gt;',
+                    "'": '&#39;',
+                    '"': '&quot;',
+                    "'": '&apos;'
+                }[tag] || tag)
+            ))
+        } catch {
+            reject('underdot >> failed to escape html')
+        }
+    })
 }
 
-var _t = function(data) {
-    document.title = data
+var _fjson = function (data) {
+    return new Promise((resolve, reject) => {
+        try {
+            resolve(JSON.stringify(data, null, "\t"))
+        } catch {
+            resolve(console.error('underdot >> failed to format json'))
+        }
+    })
 }
+
+var _l = function (url, code) {
+    return new Promise((resolve, reject) => {
+        if (typeof url == 'string' && (url.startsWith('https://') || url.startsWith('http://')) && code && typeof code == 'string') {
+            try {
+                fetch(url).then(response => response.text()).then(response => {
+                    resolve(eval(`${response}\n\n${code}`))
+                })
+            } catch {
+                reject('underdot >> failed to load script')
+            }
+        } else {
+            reject('underdot >> not a valid script url or code')
+        }
+    })
+}
+
+
+var _r = function (url) {
+    return new Promise((resolve, reject) => {
+        if (typeof url == 'string' && (url.startsWith('https://') || url.startsWith('http://'))) {
+            window.location.href = url
+            resolve(url)
+        } else {
+            reject('underdot >> not a valid url')
+        }
+    })
+}
+
+var _t = function (data) {
+    return new Promise((resolve, reject) => {
+        try {
+            document.title = data
+            resolve(data)
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
